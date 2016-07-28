3945910e7c2637bc2b83e2d269e18e875b135d28
nandhinipriyas <npriya163@gmail.com>
2016-07-28 12:07:30 +0530

    ffrrg
    frtgr

---
+++ sum.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class encry {
+
+	public static void main(String[] args){
+		 int sum=0,sum1=0,num;
+				 int a;
+				 int total=0;
+	        Scanner sc=new Scanner(System.in);
+	       
+	        num=sc.nextInt();
+	       
+	        while(num>0)
+	        {
+	        	sum=sum+(num%10);
+	        	num=num/10;
+	        	if(num!=0)
+	        	{	   a=num;
+	        	 
+	        	while(a!=0)
+	        	{
+	        		sum1=sum1+(a%10);
+	        		a=a/10;
+	        		 
+	        	}
+	        	
+	        	}
+	        	
+	            } 	        	        
+	       System.out.println(total=sum+sum1);
+	           
+	        }
+
+	}
+
+
